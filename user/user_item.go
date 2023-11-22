92f81292bbe9f6e1d476d89c2352d581f1e62901
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-11-23 06:10:00 +0700

    feat:add make new user func

--- user/user_item.go
+++ user/user_item.go
@@ -3,10 +3,14 @@ package user
 import "github.com/oklog/ulid/v2"
 
 type User struct {
-	Id ulid.ULID
+	Id   ulid.ULID
 	Name string
 }
 
-func MakeNewUser(name string) {
-	
+func MakeNewUser(name string) (User, error) {
+	u := User{
+		Id:   ulid.Make(),
+		Name: name,
+	}
+	return u, nil
 }
